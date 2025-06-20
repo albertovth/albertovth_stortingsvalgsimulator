216e48dd8fec3b278d22fd5b0d8826c32df0fb2f
Alberto Valiente Thoresen <46265925+albertovth@users.noreply.github.com>
2025-06-20 11:56:11 +0200

    Update Stortingsvalgsimulator_interaktiv.py

--- Stortingsvalgsimulator_interaktiv.py
+++ Stortingsvalgsimulator_interaktiv.py
@@ -664,7 +664,7 @@ st.title("Stortingsvalgsimulator")
 
 
 st.markdown("""
-###  Diagram og kart for prognostisert mandatfordeling presenteres nederst i hovedsiden. For å se venstremenyen: utvid med pilen til venstre. Da kan du justere prosentfordeling og valgdeltakelse per distrikt.
+###  Diagram og kart for prognostisert mandatfordeling og kart presenteres nederst i hovedsiden. For å se venstremenyen: utvid med pilen til venstre. Da kan du justere prosentfordeling og valgdeltakelse per distrikt.
 """)
 
 # Sammendrag
